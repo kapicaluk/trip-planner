eb256764cf8bdac8cedecbf70c7119fdd11015be
xifengjin88 <xifeng.jin88@gmail.com>
2017-10-03 17:40:42 -0400

    update model/index.js

--- models/index.js
+++ models/index.js
@@ -5,19 +5,58 @@ const db = new Sequelize('postgres://localhost:5432/trip-planner', {
 })
 
 const Place = db.define('page', {
-
+    address: {
+        type: Sequelize.STRING
+    },
+    city: {
+        type: Sequelize.STRING
+    },
+    state: {
+        type: Sequelize.STRING
+    },
+    phone: {
+        type: Sequelize.STRING
+    },
+    location: {
+        type: Sequelize.ARRAY(Sequelize.FLOAT)
+    }
 })
 
 const Hotel = db.define('page', {
-	
+  name: {
+      type: Sequelize.STRING
+  },
+  num_stars: {
+      type: Sequelize.FLOAT
+  },
+  amenities: {
+      type: Sequelize.STRING
+  }
 })
 
 const Restaurant = db.define('page', {
-	
-})
+	name: {
+        type: Sequelize.STRING
+    },
+    price: {
+        type: Sequelize.INTEGER
+    },
+    cuisine: {
+        type: Sequelize.INTEGER
+    }
+});
 
 const Activity = db.define('page', {
-	
-})
+	name: {
+        type: Sequelize.STRING
+    },
+    age_range: {
+        type: Sequelize.STRING
+    }
+});
+
+Place.belongsTo(Hotel);
+Place.belongsTo(Restaurant);
+Place.belongsTo(Activity);
 
 module.exports = {db, Place, Hotel, Restaurant, Activity}
