afe2c82f274df856ccbb7c39b903f8afffce1da1
xifengjin88 <xifeng.jin88@gmail.com>
2017-10-03 18:33:36 -0400

    create api

--- server/app.js
+++ server/app.js
@@ -1,20 +1,42 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
 const db = require("../models").db;
+const Hotel = require("../models").Hotel;
+const Restaurant = require("../models").Restaurant;
+const Activity = require("../models").Activity;
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 
 // logging and body-parsing
-app.use(morgan);
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get("/api", function(req, res, next) {
+  const hotels = Hotel.findAll({}).then(hotels => {
+    return hotels;
+  });
+
+  const restaurants = Restaurant.findAll({}).then(restaurants => {
+    return restaurants;
+  });
+
+  const activities = Activity.findAll({}).then(activities => {
+    return activities;
+  });
+
+  Promise.all([hotels, restaurants, activities]).then((...actractions) =>{
+    res.json(actractions);
+  }).catch(next);
+
+})
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -33,14 +55,16 @@ app.use(function(err, req, res, next) {
 
 // listen on a port
 var port = 3000;
-app.listen(port, function() {
-  console.log("The server is listening closely on port", port);
-  db
-    .sync()
-    .then(function() {
-      console.log("Synchronated the database");
-    })
-    .catch(function(err) {
-      console.error("Trouble right here in River City", err, err.stack);
-    });
-});
+
+Hotel.sync()
+.then(function () {
+    return Restaurant.sync();
+})
+.then(function () {
+    return Activity.sync();
+}).then(function() {
+  app.listen(3000, function () {
+    console.log('Server is listening on port 3000!');
+  });
+})
+
