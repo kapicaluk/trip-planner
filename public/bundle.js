d81d9b05309a551171428c78744b2f9726d3233e
kapicaluk <kapicaluk@gmail.com>
2017-10-03 16:39:04 -0400

    project start

--- public/bundle.js
+++ public/bundle.js
@@ -60,31 +60,11 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-const mapboxgl = __webpack_require__(1);
-const { Map } = mapboxgl;
-const buildMarker = __webpack_require__(3);
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2Fzc2lvemVuIiwiYSI6ImNqNjZydGl5dDJmOWUzM3A4dGQyNnN1ZnAifQ.0ZIRDup0jnyUFVzUa_5d1g';
-
-const map = new Map({
-	container: 'map',
-	center : [-74.009, 40.705], // FullStack coordinates
-	zoom: 15,
-	style: "mapbox://styles/mapbox/streets-v10"
-})
-
-const marker = buildMarker('hotels', [-74.009, 40.705])
-marker.addTo(map)
-
-/***/ }),
-/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function(global) {var require;var require;(function(f){if(true){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.mapboxgl = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return require(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
@@ -551,6 +531,26 @@ module.exports={"$version":8,"$root":{"version":{"required":true,"type":"enum","
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
 
 /***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const mapboxgl = __webpack_require__(0);
+const buildMarker = __webpack_require__(3);
+
+mapboxgl.accessToken = "YOUR API TOKEN HERE";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack coordinates
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const marker = buildMarker("activities", [-74.009, 40.705]);
+marker.addTo(map);
+
+
+/***/ }),
 /* 2 */
 /***/ (function(module, exports) {
 
@@ -581,7 +581,7 @@ module.exports = g;
 /* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const { Marker } = __webpack_require__(1);
+const { Marker } = __webpack_require__(0);
 
 const iconURLs = {
   hotels: "http://i.imgur.com/D9574Cu.png",
@@ -590,20 +590,20 @@ const iconURLs = {
 };
 
 const buildMarker = (type, coords) => {
-	// If user submits unsupported type
-	if(!iconURLs.hasOwnProperty(type)) {
-		type = 'activities'
-	}
+  if (!iconURLs.hasOwnProperty(type)) {
+    type = "activities";
+  }
+  const markerEl = document.createElement("div");
+  markerEl.style.backgroundSize = "contain";
+  markerEl.style.width = "32px";
+  markerEl.style.height = "37px";
+  markerEl.style.backgroundImage = `url(${iconURLs[type]})`;
+  return new Marker(markerEl).setLngLat(coords);
+};
 
-	const markerEl = document.createElement('div');
-	markerEl.style.width = '32px';
-	markerEl.style.height = '37px';
-	markerEl.style.backgroundImage = `url(${iconURLs[type]})`; // Look up icon based on activity
-	return new Marker(markerEl).setLngLat(coords)
+module.exports = buildMarker;
 
-}
-
-module.exports = buildMarker
 
 /***/ })
 /******/ ]);
+//# sourceMappingURL=bundle.js.map
