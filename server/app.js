5a3325898776a78f69c5ddfc408ce8d22c89f112
xifengjin88 <xifeng.jin88@gmail.com>
2017-10-04 10:05:05 -0400

    complete select and trip iternary data injection

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const db = require("../models").db;
 const Hotel = require("../models").Hotel;
 const Restaurant = require("../models").Restaurant;
 const Activity = require("../models").Activity;
+const Place = require("../models").Place;
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
@@ -19,21 +20,37 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/api", function(req, res, next) {
-  const hotels = Hotel.findAll({}).then(hotels => {
+app.get("/api/attractions", function(req, res, next) {
+  const hotels = Hotel.findAll({
+    include: [{
+      model: Place
+    }]
+  }).then(hotels => {
     return hotels;
   });
 
-  const restaurants = Restaurant.findAll({}).then(restaurants => {
+  const restaurants = Restaurant.findAll({
+    include: [{
+      model: Place
+    }]
+  }).then(restaurants => {
     return restaurants;
   });
 
-  const activities = Activity.findAll({}).then(activities => {
+  const activities = Activity.findAll({
+    include: [{
+      model: Place
+    }]
+  }).then(activities => {
     return activities;
   });
 
-  Promise.all([hotels, restaurants, activities]).then((...actractions) =>{
-    res.json(actractions);
+  Promise.all([hotels, restaurants, activities]).then(([hotels, restaurants, activities]) =>{
+    res.json({
+      hotels,
+      restaurants,
+      activities
+    });
   }).catch(next);
 
 })
@@ -48,8 +65,7 @@ app.use(function(req, res, next) {
 // handle all errors (anything passed into `next()`)
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  console.error();
-  res.send("go to hell");
+  res.send(err.message);
 });
 
 
@@ -62,6 +78,9 @@ Hotel.sync()
 })
 .then(function () {
     return Activity.sync();
+}).then(function() {
+  return Place.sync();
+  
 }).then(function() {
   app.listen(3000, function () {
     console.log('Server is listening on port 3000!');
